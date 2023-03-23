55dd60a199ccd9ff439a6aaaab3e0f83506ae843
aaronmq96 <aaronmq96@gmail.com>
2023-03-22 17:55:09 -0700

    integrated dummy frontend

--- dummy-frontend/src/App.js
+++ dummy-frontend/src/App.js
@@ -1,18 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { axios, useState } from "react";
+import { useState } from "react";
+import axios, * as others from 'axios';
 
 function App() {
 	const [file, setFile] = useState();
 
+
 	const handleFileChange = (e) => {
-		console.log(e.target);
+		console.log(e.target.files[0]);
 		setFile(e.target.files[0]);
 	};
 
-	const handleFileUpload = () => {
+	const handleFileUpload = async (event) => {
+
 		console.log(file);
+		event.preventDefault()
+		const formData = new FormData();
+		formData.append("myFile", file);
+		try {
+		  const response = await axios({
+			method: "post",
+			url: "http://localhost:8080/upload",
+			data: formData,
+			headers: { "Content-Type": "multipart/form-data" },
+		  });
+		} catch(error) {
+		  console.log(error)
+		}
 	};
+
+	const handleDownloadFile = async() =>{
+		const res = await axios("http://localhost:8080/download", {params: {fileName: "Starry Night Paint Wallpaper.png"}})
+		console.log(res)
+	}
+
 
 	return (
 		<div className="App">
@@ -23,7 +45,9 @@ function App() {
 				name="filename"
 				onChange={handleFileChange}
 			/>
-			<button type="submit">Upload File</button>
+			<button type="submit" onClick={handleFileUpload}>Upload File</button>
+
+			<button onClick={handleDownloadFile}> Download</button>
 		</div>
 	);
 }
